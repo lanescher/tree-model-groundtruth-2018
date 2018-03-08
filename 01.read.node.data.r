6e5d7c1cdb852b4cff26630bac80a428721e1dbe
lanescher <37148093+lanescher@users.noreply.github.com>
2018-03-08 11:43:15 -0600

    Create 01.read.node.data.r

---
+++ 01.read.node.data.r
@@ -0,0 +1,36 @@
+# read in node names to find corresponding nodes in multiple models
+
+# read in files
+##### change branch number here
+branch10.171221 <- read.csv("C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/branchdata/171221_branch13.txt", sep = ",", header = TRUE, skip = 1)
+branch10.180109 <- read.csv("C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/branchdata/180109_branch13.txt", sep = ",", header = TRUE, skip = 1)
+
+branch10.171221[,2:8] <- NULL
+branch10.180109[,2:8] <- NULL
+
+# find matching nodes
+branch10all <- merge(branch10.171221, branch10.180109, by = "X.Label", incomparables = TRUE)
+branch10all$node <- gsub("\\.", "*", branch10all$X.Label)
+branch10all$node <- gsub("-", ".", branch10all$node)
+# first column has all matching points
+
+# find one date only
+branch10.171221.only <- setdiff(branch10.171221$X.Label, branch10all$X.Label)
+branch10.171221.only <- as.data.frame(branch10.171221.only)
+branch10.171221.only$node <- gsub("\\.", "*", branch10.171221.only[,1])
+branch10.171221.only$node <- gsub("-", ".", branch10.171221.only$node)
+
+# find other date only
+branch10.180109.only <- setdiff(branch10.180109$X.Label, branch10all$X.Label)
+branch10.180109.only <- as.data.frame(branch10.180109.only)
+branch10.180109.only$node <- gsub("\\.", "*", branch10.180109.only[,1])
+branch10.180109.only$node <- gsub("-", ".", branch10.180109.only$node)
+
+# write all files
+
+##### change branch number here
+write.csv(branch10all, file = "C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/branchdata/branch13_both.csv")
+write.csv(branch10.171221.only, file = "C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/branchdata/branch13_171221.csv")
+write.csv(branch10.180109.only, file = "C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/branchdata/branch13_180109.csv")
+
+
