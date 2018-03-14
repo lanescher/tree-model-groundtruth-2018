e29b322fb04709d471ada2fb8acef1ed11497efe
lanescher <clanescher@gmail.com>
2018-03-14 15:50:52 -0500

    Added second model
    Added script for finding centerpoint in second model.

--- SCRIPTS/02.find.centerpoint.r
+++ SCRIPTS/02.find.centerpoint.r
@@ -1,8 +1,12 @@
 # read in centerpointdata and find center point
 
+setwd("C:/Users/lscher/Documents/Github/tree-model-groundtruth-2018")
+
+tree <- "memorialoak"
 dates <- c("171221", "180109")
 
-date1_center <- read.csv(paste("C:/Users/lscher/Documents/Tree Observatory/3D model analysis/data/centerpointdata/memorialoak_", dates[1], "_center_GPS.txt",
+# find centerpoint for date 1
+date1_center <- read.csv(paste("DATA/", tree,"_", dates[1], "_center.txt",
                                sep = ""),
                               skip = 1)
 
@@ -40,4 +44,41 @@ z <- (date1_center.new[1,3] * as.numeric(dirnum2[3]) -
 date1_centerpoint <- c(x, y, z)
 date1_center.new[5,] <- date1_centerpoint
 
-#### Repeat for date2
+# find centerpoint for date 2
+date2_center <- read.csv(paste("DATA/", tree, "_", dates[2], "_center.txt",
+                               sep = ""),
+                         skip = 1)
+
+date2_center.x <- date2_center$X_est
+date2_center.y <- date2_center$Y_est
+date2_center.z <- date2_center$Z_est
+
+date2_center.new <- as.data.frame(date2_center.x)
+date2_center.new$Y_est <- date2_center.y
+date2_center.new$Z_est <- date2_center.z
+names(date2_center.new) <- c("X_est", "Y_est", "Z_est")
+date2_center.new <- date2_center.new[15:18,]
+
+
+# calculate L, M, N
+dirnum1 <- date2_center.new[1,] - date2_center.new[2,]
+dirnum2 <- date2_center.new[3,] - date2_center.new[4,]
+
+
+# calculate x
+x <- (date2_center.new[1,1] * as.numeric(dirnum2[1]) - 
+        date2_center.new[3,1] * as.numeric(dirnum1[1])) / 
+  (as.numeric(dirnum2[1]) - as.numeric(dirnum1[1]))
+
+# calculate y
+y <- (date2_center.new[1,2] * as.numeric(dirnum2[2]) - 
+        date2_center.new[3,2] * as.numeric(dirnum1[2])) / 
+  (as.numeric(dirnum2[2]) - as.numeric(dirnum1[2]))
+
+# calculate Z
+z <- (date2_center.new[1,3] * as.numeric(dirnum2[3]) - 
+        date2_center.new[3,3] * as.numeric(dirnum1[3])) / 
+  (as.numeric(dirnum2[3]) - as.numeric(dirnum1[3]))
+
+date2_centerpoint <- c(x, y, z)
+date2_center.new[5,] <- date2_centerpoint
