35a1388cfa71ec3077368ab92d42fd4735f506e3
lanescher <clanescher@gmail.com>
2018-03-14 16:34:30 -0500

    date 2 script
    added code for date 2

--- SCRIPTS/05.find.node.theta.r
+++ SCRIPTS/05.find.node.theta.r
@@ -1,5 +1,7 @@
 # find angle of each node
 
+#Date 1
+
 # center lat and long around origin
 all.loc.data.date1$meterslat.or <- all.loc.data.date1$meterslat - date1_centerpointdf$meterslat
 all.loc.data.date1$meterslong.or <- all.loc.data.date1$meterslong - date1_centerpointdf$meterslong
@@ -10,3 +12,18 @@ all.loc.data.date1$theta <- (atan2(all.loc.data.date1$meterslat.or, all.loc.data
 
 # convert theta to degrees
 all.loc.data.date1$thetadeg <- (180/pi) * all.loc.data.date1$theta
+
+
+
+#Date 2
+
+# center lat and long around origin
+all.loc.data.date2$meterslat.or <- all.loc.data.date2$meterslat - date2_centerpointdf$meterslat
+all.loc.data.date2$meterslong.or <- all.loc.data.date2$meterslong - date2_centerpointdf$meterslong
+
+# calculate theta in radians
+all.loc.data.date2$theta <- (atan2(all.loc.data.date2$meterslat.or, all.loc.data.date2$meterslong.or) - 
+                               atan2(all.loc.data.date2$meterslat.or[1], all.loc.data.date2$meterslong.or[1]))
+
+# convert theta to degrees
+all.loc.data.date2$thetadeg <- (180/pi) * all.loc.data.date2$theta
