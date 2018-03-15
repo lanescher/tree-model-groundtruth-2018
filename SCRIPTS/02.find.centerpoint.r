b3933601270b2008d6624b14494fd44988e552d7
lanescher <clanescher@gmail.com>
2018-03-15 16:08:54 -0500

    Fixed centerpoint calculation
    Centerpoint calculation is now generalized so that the centerpoint can be found between any four points (moving in a circle)

--- SCRIPTS/02.find.centerpoint.r
+++ SCRIPTS/02.find.centerpoint.r
@@ -6,7 +6,7 @@ tree <- "memorialoak"
 dates <- c("171221", "180109")
 
 # find centerpoint for date 1
-date1_center <- read.csv(paste("DATA/", tree,"_", dates[1], "_center.txt",
+date1_center <- read.csv(paste("DATA/INPUT/", tree,"_", dates[1], "_center.txt",
                                sep = ""),
                               skip = 1)
 
@@ -18,34 +18,34 @@ date1_center.new <- as.data.frame(date1_center.x)
 date1_center.new$Y_est <- date1_center.y
 date1_center.new$Z_est <- date1_center.z
 names(date1_center.new) <- c("X_est", "Y_est", "Z_est")
-date1_center.new <- date1_center.new[17:21,]
-date1_center.new <- date1_center.new[-2,]
+date1_center.new <- date1_center.new[-c(1:14),]
 
 # calculate L, M, N
-dirnum1 <- date1_center.new[1,] - date1_center.new[2,]
-dirnum2 <- date1_center.new[3,] - date1_center.new[4,]
+dirnum1 <- date1_center.new[1,] - date1_center.new[3,]
+dirnum2 <- date1_center.new[2,] - date1_center.new[4,]
 
 
 # calculate x
 x <- (date1_center.new[1,1] * as.numeric(dirnum2[1]) - 
-        date1_center.new[3,1] * as.numeric(dirnum1[1])) / 
+        date1_center.new[2,1] * as.numeric(dirnum1[1])) / 
   (as.numeric(dirnum2[1]) - as.numeric(dirnum1[1]))
 
 # calculate y
 y <- (date1_center.new[1,2] * as.numeric(dirnum2[2]) - 
-        date1_center.new[3,2] * as.numeric(dirnum1[2])) / 
+        date1_center.new[2,2] * as.numeric(dirnum1[2])) / 
   (as.numeric(dirnum2[2]) - as.numeric(dirnum1[2]))
 
 # calculate Z
 z <- (date1_center.new[1,3] * as.numeric(dirnum2[3]) - 
-        date1_center.new[3,3] * as.numeric(dirnum1[3])) / 
+        date1_center.new[2,3] * as.numeric(dirnum1[3])) / 
   (as.numeric(dirnum2[3]) - as.numeric(dirnum1[3]))
 
 date1_centerpoint <- c(x, y, z)
 date1_center.new[5,] <- date1_centerpoint
 
+
 # find centerpoint for date 2
-date2_center <- read.csv(paste("DATA/", tree, "_", dates[2], "_center.txt",
+date2_center <- read.csv(paste("DATA/INPUT/", tree, "_", dates[2], "_center.txt",
                                sep = ""),
                          skip = 1)
 
@@ -57,28 +57,29 @@ date2_center.new <- as.data.frame(date2_center.x)
 date2_center.new$Y_est <- date2_center.y
 date2_center.new$Z_est <- date2_center.z
 names(date2_center.new) <- c("X_est", "Y_est", "Z_est")
-date2_center.new <- date2_center.new[15:18,]
+date2_center.new <- date2_center.new[-c(1:14),]
 
 
 # calculate L, M, N
-dirnum1 <- date2_center.new[1,] - date2_center.new[2,]
-dirnum2 <- date2_center.new[3,] - date2_center.new[4,]
+dirnum1 <- date2_center.new[1,] - date2_center.new[3,]
+dirnum2 <- date2_center.new[2,] - date2_center.new[4,]
 
 
 # calculate x
 x <- (date2_center.new[1,1] * as.numeric(dirnum2[1]) - 
-        date2_center.new[3,1] * as.numeric(dirnum1[1])) / 
+        date2_center.new[2,1] * as.numeric(dirnum1[1])) / 
   (as.numeric(dirnum2[1]) - as.numeric(dirnum1[1]))
 
 # calculate y
 y <- (date2_center.new[1,2] * as.numeric(dirnum2[2]) - 
-        date2_center.new[3,2] * as.numeric(dirnum1[2])) / 
+        date2_center.new[2,2] * as.numeric(dirnum1[2])) / 
   (as.numeric(dirnum2[2]) - as.numeric(dirnum1[2]))
 
 # calculate Z
 z <- (date2_center.new[1,3] * as.numeric(dirnum2[3]) - 
-        date2_center.new[3,3] * as.numeric(dirnum1[3])) / 
+        date2_center.new[2,3] * as.numeric(dirnum1[3])) / 
   (as.numeric(dirnum2[3]) - as.numeric(dirnum1[3]))
 
 date2_centerpoint <- c(x, y, z)
 date2_center.new[5,] <- date2_centerpoint
+
