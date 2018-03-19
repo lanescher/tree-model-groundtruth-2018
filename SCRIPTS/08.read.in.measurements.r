e0ca90e6ddaf9e6e11a8e7661c12ebb98a292806
lanescher <clanescher@gmail.com>
2018-03-19 15:38:38 -0500

    read measurements
    read in measurements from csv, merge with location data

--- SCRIPTS/08.read.in.measurements.r
+++ SCRIPTS/08.read.in.measurements.r
@@ -1,1 +1,42 @@
 # read in measurements
+
+tree <- "memorialoak"
+branches <- c("01", "08", "09", "10", "13")
+dates <- c("171221", "180109")
+
+all.measure <- data.frame(node = factor(),
+                                 is.in = character(),
+                                 random = numeric(),
+                                 main.nodeto = factor(),
+                                 branch.nodeto = factor(),
+                                 maindiameter.date1 = numeric(),
+                                 mainlength.date1 = numeric(),
+                                 branchdiameter.date1 = numeric(),
+                                 branchlength.date1 = numeric(),
+                                 maindiameter.date2 = numeric(),
+                                 mainlength.date2 = numeric(),
+                                 branchdiameter.date2 = numeric(),
+                                 branchlength.date2 = numeric())
+
+i <- 1
+for (i in 1:length(branches)) {
+  meas <- read.csv(paste("DATA/INPUT/branch selection_", tree, " - primary", 
+                             branches[i], ".csv", sep = ""),
+                   col.names = c("node", "is.in", "random", "main.nodeto", "branch.nodeto", 
+                                 "maindiameter.date1", "mainlength.date1",
+                                 "branchdiameter.date1", "branchlength.date1",
+                                 "maindiameter.date2", "mainlength.date2",
+                                 "branchdiameter.date2", "branchlength.date2"))
+  all.measure <- rbind(all.measure, meas)
+  i <- i + 1
+}
+
+loc.data$node <- gsub("[.]", "*", loc.data$node)
+loc.data$node <- gsub("[-]", ".", loc.data$node)
+
+# merge with location data
+all.data <- merge(loc.data, all.measure, by = "node")
+
+all.data$is.in.y <- NULL
+all.data$random <- NULL
+
