fe9382425f3a7d2cc6cc54bcb2e67785bc08f2df
lanescher <clanescher@gmail.com>
2018-03-19 16:13:37 -0500

    nothing

--- SCRIPTS/09.model.consistency.r
+++ SCRIPTS/09.model.consistency.r
@@ -1,5 +1,7 @@
 # start testing how consistent measurements are from one model to another
 
+
+
 # calculate percent error between models
 all.data$maindiameter.err <- abs((all.data$maindiameter.date1 - all.data$maindiameter.date2) / all.data$maindiameter.date1)
 all.data$mainlength.err <- abs((all.data$mainlength.date1 - all.data$mainlength.date2) / all.data$mainlength.date1)
@@ -10,3 +12,5 @@ all.data$branchlength.err <- abs((all.data$branchlength.date1 - all.data$branchl
 plot(x = all.data$hor.dist[which(all.data$main.nodeto != "end")], y = all.data$branchlength.err[which(all.data$main.nodeto != "end")])
 
 hist(all.data$branchlength.err)
+
+
