e4d6d11e0892862cf048b737f068ce2293ea711c
lanescher <clanescher@gmail.com>
2018-03-19 16:06:39 -0500

    first analysis
    made % errors and first analysis

--- SCRIPTS/09.model.consistency.r
+++ SCRIPTS/09.model.consistency.r
@@ -1,1 +1,12 @@
 # start testing how consistent measurements are from one model to another
+
+# calculate percent error between models
+all.data$maindiameter.err <- abs((all.data$maindiameter.date1 - all.data$maindiameter.date2) / all.data$maindiameter.date1)
+all.data$mainlength.err <- abs((all.data$mainlength.date1 - all.data$mainlength.date2) / all.data$mainlength.date1)
+all.data$branchdiameter.err <- abs((all.data$branchdiameter.date1 - all.data$branchdiameter.date2) / all.data$branchdiameter.date1)
+all.data$branchlength.err <- abs((all.data$branchlength.date1 - all.data$branchlength.date2) / all.data$branchlength.date1)
+
+
+plot(x = all.data$hor.dist[which(all.data$main.nodeto != "end")], y = all.data$branchlength.err[which(all.data$main.nodeto != "end")])
+
+hist(all.data$branchlength.err)
