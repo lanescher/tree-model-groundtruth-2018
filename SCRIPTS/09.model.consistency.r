0d18864e3ef2d07b79da6d7f61b27b7b3e9a571b
lanescher <clanescher@gmail.com>
2018-03-20 11:15:31 -0500

    edit script

--- SCRIPTS/09.model.consistency.r
+++ SCRIPTS/09.model.consistency.r
@@ -1,16 +1,38 @@
 # start testing how consistent measurements are from one model to another
 
-
+library(ggplot2)
 
 # calculate percent error between models
-all.data$maindiameter.err <- abs((all.data$maindiameter.date1 - all.data$maindiameter.date2) / all.data$maindiameter.date1)
-all.data$mainlength.err <- abs((all.data$mainlength.date1 - all.data$mainlength.date2) / all.data$mainlength.date1)
-all.data$branchdiameter.err <- abs((all.data$branchdiameter.date1 - all.data$branchdiameter.date2) / all.data$branchdiameter.date1)
-all.data$branchlength.err <- abs((all.data$branchlength.date1 - all.data$branchlength.date2) / all.data$branchlength.date1)
+all.data$error <- abs(all.data$value.date1 - all.data$value.date2)
+all.data$perror <- all.data$error / all.data$value.date1
 
 
-plot(x = all.data$hor.dist[which(all.data$main.nodeto != "end")], y = all.data$branchlength.err[which(all.data$main.nodeto != "end")])
+### PLOTTING
+title <- "percent error"
 
-hist(all.data$branchlength.err)
+da.ta <- all.data
 
+xvar <- all.data$measurement 
+x.lab <- "measurement type"
+
+yvar <- all.data$perror
+y.lab <- "percent error"
+
+color <- all.data$endpoint
+
+
+ggplot(data = da.ta, aes(x = xvar, y = yvar)) +
+  geom_point() +
+  labs(x = x.lab, y = y.lab) +
+  ggtitle(title) 
+
+ggplot(data = da.ta, aes(x = all.data$measurement, y = yvar)) +
+  geom_boxplot(aes(col = color)) +
+  xlab(x.lab) + ylab(y.lab) +
+  scale_y_log10(breaks = c("0.01", "1.0", "100.0"), labels = c("0.01", "1.0", "100.0")) +
+  ggtitle(title) 
+
+plot(x = all.data$hor.dist[which(all.data$main.nodeto != "end")], y = all.data$maindiameter.err[which(all.data$main.nodeto != "end")])
+
+hist(all.data$maindiameter.err)
 
