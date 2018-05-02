ae7f43b7db53f6c51e15c87089a1399468f51754
lanescher <clanescher@gmail.com>
2018-05-02 09:55:51 -0500

    added broken data

--- SCRIPTS/09.reshape.data.r
+++ SCRIPTS/09.reshape.data.r
@@ -22,7 +22,11 @@ all.data$date[which(all.data$variable == "maindiameter.date2" |
 all.data$date[which(all.data$variable == "maindiameter.real" | 
                       all.data$variable == "mainlength.real" |
                       all.data$variable == "branchdiameter.real" |
-                      all.data$variable == "branchlength.real")] <- "real"
+                      all.data$variable == "branchlength.real" |
+                      all.data$variable == "maindiameterbroken.real" |
+                      all.data$variable == "mainlengthbroken.real" |
+                      all.data$variable == "branchdiameterbroken.real" |
+                      all.data$variable == "branchlengthbroken.real")] <- "real"
 
 
 # add column for branch
@@ -32,13 +36,17 @@ all.data$branch[which(all.data$variable == "maindiameter.date1" |
                              all.data$variable == "maindiameter.date2" |
                              all.data$variable == "mainlength.date2" |
                              all.data$variable == "maindiameter.real" |
-                             all.data$variable == "mainlength.real")] <- "main"
+                             all.data$variable == "mainlength.real" |
+                             all.data$variable == "mainlengthbroken.real" |
+                             all.data$variable == "maindiameterbroken.real")] <- "main"
 all.data$branch[which(all.data$variable == "branchdiameter.date1" | 
                              all.data$variable == "branchlength.date1" |
                              all.data$variable == "branchdiameter.date2" |
                              all.data$variable == "branchlength.date2" |
                              all.data$variable == "branchdiameter.real" |
-                             all.data$variable == "branchlength.real")] <- "branch"
+                             all.data$variable == "branchlength.real"|
+                             all.data$variable == "branchlengthbroken.real" |
+                             all.data$variable == "branchdiameterbroken.real")] <- "branch"
 
 # add column for measurement
 all.data$measurement <- NA
@@ -47,13 +55,17 @@ all.data$measurement[which(all.data$variable == "maindiameter.date1" |
                                   all.data$variable == "maindiameter.date2" |
                                   all.data$variable == "branchdiameter.date2" |
                                   all.data$variable == "maindiameter.real" |
-                                  all.data$variable == "branchdiameter.real")] <- "diameter"
+                                  all.data$variable == "branchdiameter.real" |
+                                  all.data$variable == "maindiameterbroken.real" |
+                                  all.data$variable == "branchdiameterbroken.real")] <- "diameter"
 all.data$measurement[which(all.data$variable == "mainlength.date1" | 
                                   all.data$variable == "branchlength.date1" |
                                   all.data$variable == "mainlength.date2" |
                                   all.data$variable == "branchlength.date2" |
                                   all.data$variable == "mainlength.real" |
-                                  all.data$variable == "branchlength.real")] <- "length"
+                                  all.data$variable == "branchlength.real"|
+                                  all.data$variable == "mainlengthbroken.real" |
+                                  all.data$variable == "branchlengthbroken.real")] <- "length"
 
 # add column for endpoint
 all.data$endpoint <- "branch"
@@ -67,16 +79,26 @@ all.data$endpoint[which(all.data$branch == "branch" &
 date1data <- all.data[which(all.data$date == "date1"),]
 date2data <- all.data[which(all.data$date == "date2"),]
 realdata <- all.data[which(all.data$date == "real"),]
-
-
+real <- realdata[which(realdata$variable != "maindiameterbroken.real" & 
+                         realdata$variable != "mainlengthbroken.real" &
+                         realdata$variable != "branchdiameterbroken.real" &
+                         realdata$variable != "branchlengthbroken.real"),]
+realbroken <- realdata[which(realdata$variable == "maindiameterbroken.real" | 
+                         realdata$variable == "mainlengthbroken.real" |
+                         realdata$variable == "branchdiameterbroken.real" |
+                         realdata$variable == "branchlengthbroken.real"),]
 
 all.data <- merge(date1data, date2data, by = c("node", "branch", "measurement", "endpoint",
                                                "is.in.x", "vert.dist", "center.dist", "hor.dist",
                                                "theta", "main.nodeto", "branch.nodeto"))
 
-all.data <- merge(all.data, realdata, by = c("node", "branch", "measurement", "endpoint",
+all.data <- merge(all.data, real, by = c("node", "branch", "measurement", "endpoint",
                                                "is.in.x", "vert.dist", "center.dist", "hor.dist",
                                                "theta", "main.nodeto", "branch.nodeto"))
+
+all.data <- merge(all.data, realbroken, by = c("node", "branch", "measurement", "endpoint",
+                                         "is.in.x", "vert.dist", "center.dist", "hor.dist",
+                                         "theta", "main.nodeto", "branch.nodeto"))
 
 all.data$value.date1 <- as.numeric(all.data$value.x)
 all.data$value.date2 <- as.numeric(all.data$value.y)
