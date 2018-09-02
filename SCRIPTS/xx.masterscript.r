601f46fb9b0ebb1ab31f82c88bc624ac7f9ecf44
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-02 14:51:48 -0400

    change script order

--- SCRIPTS/xx.masterscript.r
+++ SCRIPTS/xx.masterscript.r
@@ -5,8 +5,6 @@
 # Produces csv files. Results are not used for subsequent analysis
 # source("../SCRIPTS/01.read.and.merge.node.lists.r")
 
-# read in centerpoint data and find centerpoint
-source("../SCRIPTS/02.find.centerpoint.r")
 
 # read in and combine all data
 source("../SCRIPTS/001.combine.all.data.r")
