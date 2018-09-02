27f8fea7a06b4ccfe95dfcdf11983ca873f330f6
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-02 15:45:41 -0400

    added to master script

--- SCRIPTS/xx.masterscript.r
+++ SCRIPTS/xx.masterscript.r
@@ -9,6 +9,20 @@
 # read in and combine all data
 source("../SCRIPTS/001.combine.all.data.r")
 
+# count measurements
+source("../SCRIPTS/14.count.measurements.r")
+
 # define error functions
 source("../SCRIPTS/10.define.error.functions.r")
 
+# calculate model consistency
+source("../SCRIPTS/11.model.consistency.r")
+
+# calculate error between model and real
+source("../SCRIPTS/12.calculate.model.real.error.r")
+
+# compare models to real
+source("../SCRIPTS/13.model.vs.real.analysis.r")
+
+# make graphs
+source("../SCRIPTS/200.graphs.for.presentation.r")
