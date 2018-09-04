cc02635bc09c6b41f0e8865e0e6c44dd7b8993bf
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-04 12:25:25 -0400

    reorder things

--- SCRIPTS/xx.masterscript.r
+++ SCRIPTS/xx.masterscript.r
@@ -12,17 +12,31 @@ source("../SCRIPTS/001.combine.all.data.r")
 # count measurements
 source("../SCRIPTS/14.count.measurements.r")
 
+# add tree branch column
+source("../SCRIPTS/15.add.tree.branch.column.r")
+
 # define error functions
 source("../SCRIPTS/10.define.error.functions.r")
-
-# calculate model consistency
-source("../SCRIPTS/11.model.consistency.r")
 
 # calculate error between model and real
 source("../SCRIPTS/12.calculate.model.real.error.r")
 
-# compare models to real
-source("../SCRIPTS/13.model.vs.real.analysis.r")
+# calculate error between two models
+source("../SCRIPTS/11a.calculate.model.consistency.r")
+
+# subset data
+source("../SCRIPTS/16.subset.data.r")
+
+
+
+
+
 
 # make graphs
 source("../SCRIPTS/200.graphs.for.presentation.r")
+
+# compare models to real ## maybe don't need this here?
+source("../SCRIPTS/13.model.vs.real.analysis.r")
+
+# calculate model consistency
+source("../SCRIPTS/11.model.consistency.r")
