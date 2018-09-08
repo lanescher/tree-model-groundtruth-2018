e6248f6871e0f92be9effa05a50ed3ad49020f43
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-08 00:31:17 -0400

    add colors and diam to length df

--- SCRIPTS/16.subset.data.r
+++ SCRIPTS/16.subset.data.r
@@ -1,6 +1,15 @@
 # subset comb.all.data
 
+comb.all.data$color <- NA
+comb.all.data$color[which(comb.all.data$tree == "memorialoak")] <- "cornflowerblue"
+comb.all.data$color[which(comb.all.data$tree == "walnut2")] <- "goldenrod"
+comb.all.data$color[which(comb.all.data$tree == "elm")] <- "brown4"
+
 d.all.data <- comb.all.data[which(comb.all.data$measurement == "diameter"),]
 l.all.data <- comb.all.data[which(comb.all.data$measurement == "length"),]
+l.all.data$diam <- d.all.data$value.real
 le <- l.all.data[which(l.all.data$endpoint == "end"),]
 lb <- l.all.data[which(l.all.data$endpoint == "branch"),]
+
+
+
