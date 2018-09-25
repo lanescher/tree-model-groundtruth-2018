040e3d2d6156826f68b012909070b02f269be045
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-25 13:59:16 -0400

    fix percent error calculation

--- SCRIPTS/06.calculate.model.consistency.r
+++ SCRIPTS/06.calculate.model.consistency.r
@@ -1,4 +1,4 @@
 # calculate percent error between models
 comb.all.data$error <- comb.all.data$value.date1 - comb.all.data$value.date2
-comb.all.data$perror <- (comb.all.data$error / comb.all.data$value.date2) * 100
+comb.all.data$perror <- (comb.all.data$error / comb.all.data$value.real) * 100
 
