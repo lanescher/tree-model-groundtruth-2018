c644769748a79027b65dd2471c4ae10a22057823
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-25 14:49:14 -0400

    updates

--- SCRIPTS/09.accuracy.analysis.r
+++ SCRIPTS/09.accuracy.analysis.r
@@ -90,7 +90,7 @@ ds <- ggplot() +
   geom_point(data=d.all.data, 
              aes(x=d.all.data$value.real, 
                  y=d.all.data$error.avg), 
-             size=1.5, color = d.all.data$color) +
+             size=2.5, color = d.all.data$color) +
   labs(x = "", y = "diameter error (cm)") +
   geom_hline(yintercept = 0, color = "grey45") +
   theme(panel.background = element_rect(fill = "white", colour = "grey50"))
@@ -99,7 +99,7 @@ ds <- ggplot() +
 ls <- ggplot() +
   geom_point(data=lb, 
              mapping=aes(x=lb$diam, y=lb$error.avg), 
-             size=1.5, color = lb$color) +
+             size=2.5, color = lb$color) +
   labs(x = "", y = "length error (cm) - node to node") +
   xlim(0,40) +
   geom_hline(yintercept = 0, color = "grey45") +
@@ -108,7 +108,7 @@ ls <- ggplot() +
 les <- ggplot() +
   geom_point(data=le, 
              mapping=aes(x=le$diam, y=le$error.avg), 
-             size=1.5, color = le$color) +
+             size=2.5, color = le$color) +
   labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
   xlim(0,18) +
   geom_hline(yintercept = 0, color = "grey45") +
@@ -129,11 +129,11 @@ ds <- ggplot() +
   geom_point(data=d.all.data, 
              aes(x=d.all.data$value.real, 
                  y=d.all.data$error.date1), 
-             size=3, color = d.all.data$color) +
+             size=2, color = d.all.data$color) +
   geom_point(data=d.all.data, 
              aes(x=d.all.data$value.real, 
                  y=d.all.data$error.date2), 
-             size=3, color = d.all.data$color) +
+             size=2, color = d.all.data$color) +
   labs(x = "", y = "diameter error (cm)") +
   geom_hline(yintercept = 0, color = "grey45") +
   theme(panel.background = element_rect(fill = "white", colour = "grey50"))
@@ -142,10 +142,10 @@ ds <- ggplot() +
 ls <- ggplot() +
   geom_point(data=lb, 
              mapping=aes(x=lb$diam, y=lb$error.date1), 
-             size=3, color = lb$color) +
+             size=2, color = lb$color) +
   geom_point(data=lb, 
              mapping=aes(x=lb$diam, y=lb$error.date2), 
-             size=3, color = lb$color) +
+             size=2, color = lb$color) +
   labs(x = "", y = "length error (cm) - node to node") +
   xlim(0,40) +
   geom_hline(yintercept = 0, color = "grey45") +
@@ -154,11 +154,11 @@ ls <- ggplot() +
 les <- ggplot() +
   geom_point(data=le, 
              mapping=aes(x=le$diam, y=le$error.date1), 
-             size=3, color = le$color) +
+             size=2, color = le$color) +
   labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
   geom_point(data=le, 
              mapping=aes(x=le$diam, y=le$error.date2), 
-             size=3, color = le$color) +
+             size=2, color = le$color) +
   labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
   xlim(0,18) +
   geom_hline(yintercept = 0, color = "grey45") +
