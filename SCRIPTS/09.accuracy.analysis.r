8d7130bd82ca383787106831b59380606d95d439
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-25 14:13:31 -0400

    separate points

--- SCRIPTS/09.accuracy.analysis.r
+++ SCRIPTS/09.accuracy.analysis.r
@@ -90,7 +90,7 @@ ds <- ggplot() +
   geom_point(data=d.all.data, 
              aes(x=d.all.data$value.real, 
                  y=d.all.data$error.avg), 
-             size=1, color = d.all.data$color) +
+             size=1.5, color = d.all.data$color) +
   labs(x = "", y = "diameter error (cm)") +
   geom_hline(yintercept = 0, color = "grey45") +
   theme(panel.background = element_rect(fill = "white", colour = "grey50"))
@@ -99,7 +99,7 @@ ds <- ggplot() +
 ls <- ggplot() +
   geom_point(data=lb, 
              mapping=aes(x=lb$diam, y=lb$error.avg), 
-             size=1, color = lb$color) +
+             size=1.5, color = lb$color) +
   labs(x = "", y = "length error (cm) - node to node") +
   xlim(0,40) +
   geom_hline(yintercept = 0, color = "grey45") +
@@ -108,7 +108,7 @@ ls <- ggplot() +
 les <- ggplot() +
   geom_point(data=le, 
              mapping=aes(x=le$diam, y=le$error.avg), 
-             size=1, color = le$color) +
+             size=1.5, color = le$color) +
   labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
   xlim(0,18) +
   geom_hline(yintercept = 0, color = "grey45") +
@@ -116,6 +116,56 @@ les <- ggplot() +
 
 
 jpeg("../OUT/FIGURE.diam.length.accuracy.size-nolines.jpg", width = 500, height = 1000)
+ggarrange(ds, ls, les, ncol = 1, nrow = 3, labels = c("A", "B", "C"),
+          common.legend = TRUE)
+dev.off()
+
+
+
+# do it without lines ----
+ds <- ggplot() +
+  xlim(0,40) +
+  ylim(-5, 15) + 
+  geom_point(data=d.all.data, 
+             aes(x=d.all.data$value.real, 
+                 y=d.all.data$error.date1), 
+             size=3, color = d.all.data$color) +
+  geom_point(data=d.all.data, 
+             aes(x=d.all.data$value.real, 
+                 y=d.all.data$error.date2), 
+             size=3, color = d.all.data$color) +
+  labs(x = "", y = "diameter error (cm)") +
+  geom_hline(yintercept = 0, color = "grey45") +
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"))
+
+
+ls <- ggplot() +
+  geom_point(data=lb, 
+             mapping=aes(x=lb$diam, y=lb$error.date1), 
+             size=3, color = lb$color) +
+  geom_point(data=lb, 
+             mapping=aes(x=lb$diam, y=lb$error.date2), 
+             size=3, color = lb$color) +
+  labs(x = "", y = "length error (cm) - node to node") +
+  xlim(0,40) +
+  geom_hline(yintercept = 0, color = "grey45") +
+  theme(panel.background = element_rect(fill = "white", colour = "grey50")) 
+
+les <- ggplot() +
+  geom_point(data=le, 
+             mapping=aes(x=le$diam, y=le$error.date1), 
+             size=3, color = le$color) +
+  labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
+  geom_point(data=le, 
+             mapping=aes(x=le$diam, y=le$error.date2), 
+             size=3, color = le$color) +
+  labs(x = "branch diameter (cm)", y = "length error (cm) - node to end") +
+  xlim(0,18) +
+  geom_hline(yintercept = 0, color = "grey45") +
+  theme(panel.background = element_rect(fill = "white", colour = "grey50")) 
+
+
+jpeg("../OUT/FIGURE.diam.length.accuracy.size-nolines2.jpg", width = 500, height = 1000)
 ggarrange(ds, ls, les, ncol = 1, nrow = 3, labels = c("A", "B", "C"),
           common.legend = TRUE)
 dev.off()
