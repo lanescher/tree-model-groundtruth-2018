21305990ce14ce55cb890c6579754282e139843f
lanescher <37148093+lanescher@users.noreply.github.com>
2018-09-27 09:59:50 -0400

    add significance stars

--- SCRIPTS/10.weather.conditions.analysis.r
+++ SCRIPTS/10.weather.conditions.analysis.r
@@ -42,72 +42,6 @@ names(ttest) <- c("", "Diameter", "Length", "Length, to branch", "Length, to end
 write.csv(ttest, "../OUT/TABLE.light.conditions.pvalues.absvalueerror.csv")
 
 
-# add size class column
-d.all.weather$sizeclass <- NA
-d.all.weather$sizeclass[which(d.all.weather$value.real < 5)] <- "<5"
-d.all.weather$sizeclass[which(d.all.weather$value.real >= 5 & 
-                                d.all.weather$value.real < 10)] <- "5 - 10"
-d.all.weather$sizeclass[which(d.all.weather$value.real >= 10 & 
-                                d.all.weather$value.real < 20)] <- "10 - 20"
-d.all.weather$sizeclass[which(d.all.weather$value.real >= 20 & 
-                                d.all.weather$value.real < 30)] <- "20 - 30"
-d.all.weather$sizeclass[which(d.all.weather$value.real >= 30)] <- ">30"
-d.all.weather$sizeclass <- factor(d.all.weather$sizeclass, levels = c("<5", "5 - 10", "10 - 20", "20 - 30", ">30"))
-
-
-
-l.all.weather$sizeclass <- NA
-l.all.weather$sizeclass[which(l.all.weather$value.real < 5)] <- "<5"
-l.all.weather$sizeclass[which(l.all.weather$value.real >= 5 & 
-                                l.all.weather$value.real < 10)] <- "5 - 10"
-l.all.weather$sizeclass[which(l.all.weather$value.real >= 10 & 
-                                l.all.weather$value.real < 20)] <- "10 - 20"
-l.all.weather$sizeclass[which(l.all.weather$value.real >= 20 & 
-                                l.all.weather$value.real < 30)] <- "20 - 30"
-l.all.weather$sizeclass[which(l.all.weather$value.real >= 30)] <- ">30"
-l.all.weather$sizeclass <- factor(l.all.weather$sizeclass, levels = c("<5", "5 - 10", "10 - 20", "20 - 30", ">30"))
-
-le.weather$sizeclass <- NA
-le.weather$sizeclass[which(le.weather$value.real < 5)] <- "<5"
-le.weather$sizeclass[which(le.weather$value.real >= 5 & 
-                             le.weather$value.real < 10)] <- "5 - 10"
-le.weather$sizeclass[which(le.weather$value.real >= 10 & 
-                             le.weather$value.real < 20)] <- "10 - 20"
-le.weather$sizeclass[which(le.weather$value.real >= 20 & 
-                             le.weather$value.real < 30)] <- "20 - 30"
-le.weather$sizeclass[which(le.weather$value.real >= 30)] <- ">30"
-le.weather$sizeclass <- factor(le.weather$sizeclass, levels = c("<5", "5 - 10", "10 - 20", "20 - 30", ">30"))
-
-
-lb.weather$sizeclass <- NA
-lb.weather$sizeclass[which(lb.weather$value.real < 5)] <- "<5"
-lb.weather$sizeclass[which(lb.weather$value.real >= 5 & 
-                             lb.weather$value.real < 10)] <- "5 - 10"
-lb.weather$sizeclass[which(lb.weather$value.real >= 10 & 
-                             lb.weather$value.real < 20)] <- "10 - 20"
-lb.weather$sizeclass[which(lb.weather$value.real >= 20 & 
-                             lb.weather$value.real < 30)] <- "20 - 30"
-lb.weather$sizeclass[which(lb.weather$value.real >= 30)] <- ">30"
-lb.weather$sizeclass <- factor(lb.weather$sizeclass, levels = c("<5", "5 - 10", "10 - 20", "20 - 30", ">30"))
-
-
-d <- ggplot(d.all.weather, aes(x = tree, y = error.x, fill = interaction(weather, sizeclass, tree))) +
-  geom_boxplot() +
-  theme(legend.position = "none")
-
-lb <- ggplot(lb.weather, aes(x = tree, y = error.x, fill = interaction(weather, sizeclass, tree))) +
-  geom_boxplot() +
-  theme(legend.position = "none")
-
-le <- ggplot(le.weather, aes(x = tree, y = error.x, fill = interaction(weather, sizeclass, tree))) +
-  geom_boxplot() +
-  theme(legend.position = "none")
-
-jpeg("../OUT/FIGURE.lightconditions.error.size.jpg", width = 400, height = 1000)
-ggarrange(d, lb, le, labels = c("A", "B", "C"),
-          ncol = 1, nrow = 3,
-          align = "hv")
-dev.off()
 
 
 darken <- function(color, factor=1.25){
@@ -125,63 +59,75 @@ lighten <- function(color, factor=1.25){
 }
 
 
-d <- ggplot(data = d.all.weather, aes(x = tree, y = perror.x, 
+ggplot(data = d.all.weather, aes(x = tree, y = abs(error.x), 
                                  fill = interaction(tree, weather))) +
   geom_boxplot() +
   scale_fill_manual(values = c(darken("brown4", 1.1), darken("cornflowerblue"), 
                                darken("goldenrod2"), 
                                lighten("brown4"), lighten("cornflowerblue"),
                                lighten("goldenrod2"))) +
-  labs(x = "", y = "absolute error of diameters") +
+  labs(x = "", y = "error of diameters (cm)") +
   theme(legend.position = "none", 
         panel.background = element_rect(fill = "white", colour = "grey50")) +
-  geom_hline(yintercept = 0, color = "grey60")
+  geom_hline(yintercept = 0, color = "grey60") +
+  stat_compare_means(aes(label = ..p.signif..),
+                     method = "t.test",
+                     symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), 
+                                        symbols = c("****", "***", "**", "*", "")))
 
 
 
-ggplot(data = l.all.weather, aes(x = tree, y = perror.x, 
+l <- ggplot(data = l.all.weather, aes(x = tree, y = abs(error.x), 
                                  fill = interaction(tree, weather))) +
   geom_boxplot() +
   scale_fill_manual(values = c(darken("brown4", 1.1), darken("cornflowerblue"), 
                                darken("goldenrod2"), 
                                lighten("brown4"), lighten("cornflowerblue"),
                                lighten("goldenrod2")))  +
-  labs(x = "", y = "absolute error of lengths") +
+  labs(x = "", y = "error of lengths (cm)") +
   theme(legend.position = "none", 
         panel.background = element_rect(fill = "white", colour = "grey50")) +
-  geom_hline(yintercept = 0, color = "grey60")
+  geom_hline(yintercept = 0, color = "grey60") +
+  stat_compare_means(aes(label = ..p.signif..),
+                     method = "t.test")
 
 
 
-lb <- ggplot(data = lb.weather, aes(x = tree, y = perror.x,
+lb <- ggplot(data = lb.weather, aes(x = tree, y = abs(error.x),
                               fill = interaction(tree, weather))) +
   geom_boxplot() +
   scale_fill_manual(values = c(darken("brown4", 1.1), darken("cornflowerblue"), 
                                darken("goldenrod2"), 
                                lighten("brown4"), lighten("cornflowerblue"),
                                lighten("goldenrod2")))  +
-  labs(x = "", y = "absolute error of lengths, to branch") +
+  labs(x = "", y = "error of lengths, node to node (cm)") +
   theme(legend.position = "none", 
         panel.background = element_rect(fill = "white", colour = "grey50")) +
-  geom_hline(yintercept = 0, color = "grey60")
+  geom_hline(yintercept = 0, color = "grey60") +
+  stat_compare_means(aes(label = ..p.signif..),
+                     method = "t.test")
 
 
 
-le <- ggplot(data = le.weather, aes(x = tree, y = perror.x,
+le <- ggplot(data = le.weather, aes(x = tree, y = abs(error.x),
                               fill = interaction(tree, weather))) +
   geom_boxplot() +
   scale_fill_manual(values = c(darken("brown4", 1.1), darken("cornflowerblue"), 
                                darken("goldenrod2"), 
                                lighten("brown4"), lighten("cornflowerblue"),
                                lighten("goldenrod2")))  +
-  labs(x = "", y = "absolute error of lengths, to end") +
+  labs(x = "", y = "error of lengths, node to end (cm)") +
   theme(legend.position = "none", 
         panel.background = element_rect(fill = "white", colour = "grey50")) +
-  geom_hline(yintercept = 0, color = "grey60")
+  geom_hline(yintercept = 0, color = "grey60") +
+  stat_compare_means(aes(label = ..p.signif..),
+                     method = "t.test",
+                     symnum.args = list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1), 
+                                        symbols = c("****", "***", "**", "*", "")))
 
 
 figure <- ggarrange(d, lb, le, labels = c("A", "B", "C"),
           ncol = 1, nrow = 3,
           align = "hv")
-ggsave(plot = figure, "../OUT/FIGURE.lightconditions.error-percent.jpg",
+ggsave(plot = figure, "../OUT/FIGURE.lightconditions.error.jpg",
        width = 4, height = 10)
